a9bf6b0eb8d1317fce73b92ca811c58e2d9d12c8
Chanlito Hem <chanlitohem@gmail.com>
2026-02-03 22:54:39 +0700

    feat(config): ✨ integrate Tailwind CSS and Shadcn components into Nuxt configuration

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex min-h-screen items-center justify-center p-4">
+    <Card class="w-full max-w-md">
+      <CardHeader>
+        <CardTitle>Hello World!</CardTitle>
+      </CardHeader>
+      <CardFooter class="justify-end">
+        <Button>Learn more</Button>
+      </CardFooter>
+    </Card>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
   devtools: { enabled: true },
   future: { compatibilityVersion: 4 },
   compatibilityDate: '2025-07-15',
@@ -8,5 +8,19 @@ export default defineNuxtConfig({
     config: {
       stylistic: true,
     },
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: '@/components/ui',
   },
 })
