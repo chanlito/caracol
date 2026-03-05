2fbe6d49d9febef8d78b1666e2b0c0812ecde51a
Chanlito Hem <chanlitohem@gmail.com>
2026-03-05 09:49:37 +0700

    feat(auth): 🔐 add authentication flow and tests

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :caracol, :scopes,
+  user: [
+    default: true,
+    module: Caracol.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :binary_id,
+    schema_table: :users,
+    test_data_fixture: Caracol.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :caracol,
   ecto_repos: [Caracol.Repo],
   generators: [timestamp_type: :utc_datetime, binary_id: true]
